02bc8ce05f9b1c718e1543af66af6035eed0a881
magaum <weslei.paula@fatec.sp.gov.br>
2021-07-25 11:31:15 -0300

    refactor: estrutura do projeto refatorada

---
+++ ecs/src/Contagem.Api.Test/ExampleTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Test.Contagem
+{
+    public class ExampleTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
